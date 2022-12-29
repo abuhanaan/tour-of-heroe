85c6ff28cd2b95b275129af80f2d466ce57dc4c8
Mustopha Qomorudeen <adewunmi99@gmail.com>
2022-12-29 11:03:44 +0100

    shell navigations

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,9 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
 import { HeroesComponent } from './components/heroes/heroes.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
 
 const routes: Routes = [
-  { path: 'heroes', component: HeroesComponent}
+  { path: 'heroes', component: HeroesComponent},
+  { path: 'dashboard', component: DashboardComponent},
+  { path: '', redirectTo: '/dashboard', pathMatch: 'full'}
 ]
 
 @NgModule({
