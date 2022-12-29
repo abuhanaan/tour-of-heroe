ccd1bc9cd18b2913bb522a3137eedc215685e61c
Mustopha Qomorudeen <adewunmi99@gmail.com>
2022-12-29 11:09:28 +0100

    hero detail route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,8 +3,10 @@ import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
 import { HeroesComponent } from './components/heroes/heroes.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { HeroDetailComponent } from './components/hero-detail/hero-detail.component';
 
 const routes: Routes = [
+  { path: 'details/:id', component: HeroDetailComponent },
   { path: 'heroes', component: HeroesComponent},
   { path: 'dashboard', component: DashboardComponent},
   { path: '', redirectTo: '/dashboard', pathMatch: 'full'}
